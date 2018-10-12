aabcfe000be88d9387042a72a101db1ee38591fd
Jean Lange <langeje@us.ibm.com>
2018-10-12 10:21:54 -0400

    fetch and display list of movies

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    movies: []
+  };
+
+  componentDidMount() {
+    console.log('I am mounting!');
+    // go get data from the movies API
+    axios.get(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b6fbc7f3f313bd395902af464ef47262`)
+      .then(res => {
+        console.log(res);
+        const movies = res.data.results;
+        this.setState({ movies });
+      })
+  }
+
   render() {
+    console.log('rendering!');
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello, world!
           </p>
+          <ul>
+            { this.state.movies.map(movie => <li key={movie.id}>{movie.title}</li>) }
+          </ul>
           <a
             className="App-link"
             href="https://reactjs.org"
